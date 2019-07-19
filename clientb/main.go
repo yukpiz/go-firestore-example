76247a858e219a4cfa8212698f0429214057ab77
yukpiz <yukpiz@gmail.com>
2019-07-19 11:39:18 +0900

    append clients

---
+++ clientb/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"strings"
+	"time"
+
+	firebase "firebase.google.com/go"
+	"github.com/yukpiz/go-firestore-example/domain"
+	"golang.org/x/xerrors"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	msg := flag.String("msg", "", "message")
+	flag.Parse()
+	if len(strings.TrimSpace(*msg)) == 0 {
+		panic(xerrors.New("empty message[required -msg option]"))
+	}
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("yukpiz-labo.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		panic(err)
+	}
+
+	cli, err := app.Firestore(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+	ref, res, err := cli.Collection("messages").Add(ctx, &domain.Message{
+		Message:  *msg,
+		SenderID: "clientb",
+		SentAt:   time.Now().Unix(),
+	})
+	log.Println(ref, res)
+}
